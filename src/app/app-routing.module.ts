a29159b37879888334a917634be38bd6a8dca246
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2023-05-30 20:28:44 +0200

    Add simple page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { HomeComponent } from './modules/home/home.component';
+import { CompanyComponent } from './modules/company/company.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: DefaultComponent, children: [
+      {path: '', component: HomeComponent},
+      {path: 'company', component: CompanyComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
