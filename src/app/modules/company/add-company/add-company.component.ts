8a3f608ca64d858494d88e90af31121da3c5177c
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2023-08-29 13:01:59 +0200

    Add validator nip to add company form.

--- src/app/modules/company/add-company/add-company.component.ts
+++ src/app/modules/company/add-company/add-company.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { validatePolish } from 'validate-polish';
 
 @Component({
   selector: 'app-add-company',
@@ -22,7 +23,7 @@ export class AddCompanyComponent {
   wwwSite!: FormControl;
   wwwStore!: FormControl;
   validationErrors = new Map<string, String>();
-  legalFormList: string[] = Array.of("JDG", "SC", "SJ", "SP", "SK", "SKA", "ZOO", "PSA", "SA");
+  legalFormList: Map<string, string> = this.createLegalFormList();
 
   constructor(
     private formBuilder: FormBuilder,
@@ -37,7 +38,7 @@ export class AddCompanyComponent {
   createRegistrationFormControls() {
     this.name = new FormControl('', [Validators.required, Validators.minLength(2)]);
     this.slug = new FormControl('', [Validators.required, Validators.minLength(2)]);
-    this.nip = new FormControl('', [Validators.required, Validators.minLength(2)]);
+    this.nip = new FormControl('', [Validators.required]);
     this.regon = new FormControl('', [Validators.required, Validators.minLength(2)]);
     this.krs = new FormControl('', [Validators.required, Validators.minLength(2)]);
     this.email = new FormControl('', [Validators.required, Validators.minLength(2)]);
@@ -59,10 +60,28 @@ export class AddCompanyComponent {
       phone: this.phone,
       wwwSite: this.wwwSite,
       wwwStore: this.wwwStore,
-    })
+    }, {validators: this.nipIsValid})
   }
 
   addCompany() {
 
+  }
+
+  createLegalFormList(): Map<string, string> {
+    let legalFormMap = new Map<string, string>();
+    legalFormMap.set("JDG", "Jednoosobowa działalność gospodarcza");
+    legalFormMap.set("SC", "Spółka cywilna");
+    legalFormMap.set("SJ", "Spółka jawna");
+    legalFormMap.set("SP", "Spółka partnerska");
+    legalFormMap.set("SK", "Spółka komandytowa");
+    legalFormMap.set("SKA", "Spółka komandytowo-akcyjna");
+    legalFormMap.set("ZOO", "Spółka z ograniczoną odpowiedzialnością");
+    legalFormMap.set("PSA", "Prosta spółka akcyjna");
+    legalFormMap.set("SA", "Spółka akcyjna");
+    return legalFormMap;
+  }
+
+  public nipIsValid(c: AbstractControl): {nipValid: boolean} | null {
+    return validatePolish.nip(c.value.nip) ? null : {nipValid: true};
   }
 }
