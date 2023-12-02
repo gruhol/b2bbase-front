0dace88d0188d235923f65829e4ce6c353c76b0c
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2023-12-02 16:37:53 +0100

    Add filtrs to catalogs.

--- src/app/modules/catalog/company-catalog2/company-catalog2.component.ts
+++ src/app/modules/catalog/company-catalog2/company-catalog2.component.ts
@@ -9,10 +9,11 @@ import { CompanyCatalog } from '../dto/CompanyCatalog';
 import { PageEvent } from '@angular/material/paginator';
 import { CatalogService } from '../catalog-service';
 import { CategoryCatalog } from '../dto/CategoryCatalog';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 interface CategoryNode {
   name: string;
-  id?: number;
+  id: number;
   slug: string;
   children?: CategoryNode[];
   selected?: boolean;
@@ -33,6 +34,10 @@ export class CompanyCatalog2Component {
   public showOnlySelected = false;
   page!: Page<CompanyCatalog>;
   voivodeship!: Map<string, string>;
+  voivodeshipCheckedList: string[] = [];
+  public isEdiCooperation: boolean | undefined;
+  public isApiCooperation: boolean | undefined;
+  public isProductFileCooperation: boolean | undefined;
 
   constructor(
     private snackBar: MatSnackBar,
@@ -40,7 +45,7 @@ export class CompanyCatalog2Component {
     private catalogService: CatalogService
   ) {
     this.catalogService.getCategory2()
-      .pipe(map(node => this.mapCategoryResponsesToVehicleNodes(node)))
+      .pipe(map(node => this.mapCategoryResponsesToCategoryNode(node)))
       .subscribe(data => {
         this.categoryDataSource.data = data;
         for(let i = 0; i < this.categoryDataSource.data.length; i++) {
@@ -68,35 +73,33 @@ export class CompanyCatalog2Component {
 
   createVoivodeshipList(): Map<string, string> {
     let voivodeshipMap = new Map<string, string>();
-    voivodeshipMap.set("DS","dolnośląskie"),
-    voivodeshipMap.set("KP", "kujawsko-pomorskie"),
-    voivodeshipMap.set("LU", "lubelskie"),
-    voivodeshipMap.set("LB", "lubuskie"),
-    voivodeshipMap.set("LD", "łódzkie"),
-    voivodeshipMap.set("MA", "małopolskie"),
-    voivodeshipMap.set("MZ", "mazowieckie"),
-    voivodeshipMap.set("OP", "opolskie"),
-    voivodeshipMap.set("PK", "podkarpackie"),
-    voivodeshipMap.set("PD", "podlaskie"),
-    voivodeshipMap.set("PM", "pomorskie"),
-    voivodeshipMap.set("SL", "śląskie"),
-    voivodeshipMap.set("SK", "świętokrzyskie"),
-    voivodeshipMap.set("WN", "warmińsko-mazurskie"),
-    voivodeshipMap.set("WP", "wielkopolskie"),
-    voivodeshipMap.set("ZP", "zachodniopomorskie");
+    voivodeshipMap.set("dolnoslaskie","dolnośląskie"),
+    voivodeshipMap.set("kujawsko-pomorskie", "kujawsko-pomorskie"),
+    voivodeshipMap.set("lubelskie", "lubelskie"),
+    voivodeshipMap.set("lubuskie", "lubuskie"),
+    voivodeshipMap.set("lodzkie", "łódzkie"),
+    voivodeshipMap.set("malopolskie", "małopolskie"),
+    voivodeshipMap.set("mazowieckie", "mazowieckie"),
+    voivodeshipMap.set("opolskie", "opolskie"),
+    voivodeshipMap.set("podkarpackie", "podkarpackie"),
+    voivodeshipMap.set("podlaskie", "podlaskie"),
+    voivodeshipMap.set("pomorskie", "pomorskie"),
+    voivodeshipMap.set("slaskie", "śląskie"),
+    voivodeshipMap.set("swietokrzyskie", "świętokrzyskie"),
+    voivodeshipMap.set("warminsko-mazurskie", "warmińsko-mazurskie"),
+    voivodeshipMap.set("wielkopolskie", "wielkopolskie"),
+    voivodeshipMap.set("zachodniopomorskie", "zachodniopomorskie");
     return voivodeshipMap;
   }
 
-  private mapCategoryResponsesToVehicleNodes(category: CategoryCatalog[]): CategoryNode[] {
+  private mapCategoryResponsesToCategoryNode(category: CategoryCatalog[]): CategoryNode[] {
     return category.map(cat => {
 
       var category: CategoryNode = {
         name: cat.name,
         slug: cat.slug,
         id: cat.id,
-        children: (cat.children) ? this.mapCategoryResponsesToVehicleNodes(cat.children): [],
-        //selected: categoryResponse.selected,
-        //indeterminate: (categoryResponse.children) ? categoryResponse.children.some(child => child.selected) : false
+        children: (cat.children) ? this.mapCategoryResponsesToCategoryNode(cat.children): [],
       };
       return category;
     });
@@ -144,6 +147,47 @@ export class CompanyCatalog2Component {
       .getDescendants(node)
       .filter((node) => node.children == null || node.children.length === 0)
       .every((node) => this.hideLeafNode(node));
+  }
+
+  tescik() {
+    console.log(this.categoryDataSource);
+    console.log(this.voivodeshipCheckedList);
+    let selectCategory = this.categoryDataSource.data.reduce(
+      (acc: number[], node: CategoryNode) =>
+        acc.concat(
+          this.treeControl
+            .getDescendants(node)
+            .filter(
+              (node) =>
+                (node.children == null || node.children.length === 0) &&
+                node.selected &&
+                !this.hideLeafNode(node)
+            )
+            .map((descendant) => descendant.id || 0)
+        ),
+      [] as number[]
+    );
+    console.log(selectCategory);
+    console.log("Edi: " + this.isEdiCooperation);
+    console.log("Api: " + this.isApiCooperation);
+    console.log("File: " + this.isProductFileCooperation);
+  }
+
+  toggleVoivodeship(key: string, isChecked: boolean) {
+    if (isChecked) {
+      this.voivodeshipCheckedList.push(key);
+    } else {
+      const index = this.voivodeshipCheckedList.indexOf(key);
+      if (index !== -1) {
+        this.voivodeshipCheckedList.splice(index, 1);
+      }
+    }
+  }
+
+  toggleCooperation(fieldName: string, event: any) {
+    if (fieldName === 'isEdiCooperation') this.isEdiCooperation = event.checked;
+    if (fieldName === 'isApiCooperation') this.isApiCooperation = event.checked;
+    if (fieldName === 'isProductFileCooperation') this.isProductFileCooperation = event.checked;
   }
 
 }
