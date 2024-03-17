4fd6e9eb97916e55fff96d19e84d65f9a5dc775c
gruhol <dabrowskiw@gmail.com>
2024-03-17 21:12:03 +0100

    Add download sitemap file.

--- src/app/layouts/sitemap/sitemap.component.ts
+++ src/app/layouts/sitemap/sitemap.component.ts
@@ -8,17 +8,21 @@ import { SitemapService } from './sitemap.service';
 export class SitemapComponent {
   constructor(private siteMapService: SitemapService) { }
 
-  downloadXml() {
-    this.siteMapService.downloadXml().subscribe(data => {
-      const xmlBlob = new Blob([data], { type: 'application/xml' });
-      const url = window.URL.createObjectURL(xmlBlob);
-      window.open(url);
-    });
-  }
-
   ngOnInit(): void {
-    this.downloadXml();
+    this.downloadXML();
   }
 
-  
+  downloadXML() {
+    fetch('api/sitemap.xml')
+    .then(response => response.text())
+    .then(xmlData => {
+        const element = document.createElement('a');
+        const file = new Blob([xmlData], {type: 'application/xml'});
+        element.href = URL.createObjectURL(file);
+        element.download = 'sitemap.xml';
+        document.body.appendChild(element);
+        element.click();
+    })
+    .catch(error => console.error('Błąd pobierania pliku XML:', error));
+  }
 }
