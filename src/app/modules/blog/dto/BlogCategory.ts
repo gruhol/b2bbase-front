42da523158b655ad7e2e803442bdcc20d690e3ed
gruhol <dabrowskiw@gmail.com>
2024-04-06 14:11:05 +0200

    Revert "Add blog service."
    This reverts commit 11391fef25ab02461be5c94cc1dbcfdec77f6a21.

--- src/app/modules/blog/dto/BlogCategory.ts
+++
@@ -1,6 +0,0 @@
-export interface BlogCategory {
-    id: number,
-    name: string,
-    slug: string,
-    description: string,
-}
--- src/app/modules/blog/dto/UserResponse.ts
+++
@@ -1,6 +0,0 @@
-export interface UserResponse {
-    firstName: string,
-    lastName: string,
-    username: string,
-    phone: string
-}
